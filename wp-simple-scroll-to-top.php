59abfe58f1c8e2043350bc7de84a90edd342698d
Äntor <ntormahmud789@gmail.com>
2024-10-20 17:02:06 +0600

    Added to css file, js file & Images

---
+++ wp-simple-scroll-to-top.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* Plugin Name:          Wp Simple Scroll to Top
+* Plugin URI:           https://wordpress.org/plugins/wp-simple-scroll-to-top/
+* Description:          A simple scroll to top button for your website.
+* Version:              1.0.0
+* requires at least:    5.2
+* requires PHP:         7.2
+* Author:               MD Mahmudul Hasan
+* Author URI:           https://mahmudulhasan13.com/
+* License:              GPL v2 or later
+* License URI:          https://www.gnu.org/licenses/gpl-2.0.html
+*update_url:            https://github.com/antor9732
+*text-domain:           wsstt
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
