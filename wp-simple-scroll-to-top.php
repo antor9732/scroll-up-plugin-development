ee0ac7dece62154d4e764c1a5b1e39fc3c94a700
Äntor <ntormahmud789@gmail.com>
2024-10-20 18:27:26 +0600

    Including css,js & Plugin Activation Function

--- wp-simple-scroll-to-top.php
+++ wp-simple-scroll-to-top.php
@@ -10,25 +10,33 @@
 * Author URI:           https://mahmudulhasan13.com/
 * License:              GPL v2 or later
 * License URI:          https://www.gnu.org/licenses/gpl-2.0.html
-*update_url:            https://github.com/antor9732
-*text-domain:           wsstt
+* update_url:           https://github.com/antor9732
+* text-domain:          wsstt
 */
 
+// Including css 
+function wsstt_enqueue_style(){
+    wp_enqueue_style('wsstt-style',plugins_url('css/wsstt-style.css',__FILE__));
+}
+add_action('wp_enqueue_scripts', 'wsstt_enqueue_style');
 
+// Including js
+function wsstt_enqueue_scripts(){
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('wsstt-plugin-script', plugins_url('js/wsstt-plugin.js', __FILE__), array(), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'wsstt_enqueue_scripts');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Including Plugin Settings Activation
+function wsstt_scroll_script(){
+    ?>
+    <script>
+        jQuery(document).ready(function(){
+            jQuery.scrollUp();
+        });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'wsstt_scroll_script');
 
 ?>
